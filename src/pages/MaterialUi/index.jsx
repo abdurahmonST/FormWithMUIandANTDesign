7b1fe6496c638963fb61c29f242a21f07359de28
Abdurahmon <abdurahmonbekmirzayev3@gmail.com>
2025-07-22 14:27:15 +0500

    first commit

---
+++ src/pages/MaterialUi/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FormPageMaterialUi = () => {
+  return (
+    <div>
+      <nav>Multi Step Form</nav>
+    </div>
+  );
+};
+
+export default FormPageMaterialUi;
